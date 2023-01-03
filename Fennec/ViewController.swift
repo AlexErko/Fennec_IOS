a63da6cfca0a873e19b77ac56491f54c158d98e5
AlexErko <profile.aleksey@gmail.com>
2023-01-03 14:48:28 +0200

    Created Xcode project

---
+++ Fennec/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Fennec
+//
+//  Created by Алексей Ерко on 03.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
