f305545ba979b8135bcc9c4304bf80b1c85fe610
AlexErko <profile.aleksey@gmail.com>
2023-01-06 12:13:51 +0200

    Fixed rootViewController in scene delegate

--- Fennec/SceneDelegate.swift
+++ Fennec/SceneDelegate.swift
@@ -14,10 +14,8 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let scene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: scene)
-        window?.rootViewController = UIViewController()
+        window?.rootViewController = ViewController()
         window?.makeKeyAndVisible()
-        
-        
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
